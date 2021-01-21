fcb8616505305ae5a81f7be5c600c89f5931171d
Clodoaldo Junior <clodoaldo.junior@ymail.com>
2021-01-21 00:04:28 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioLucas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExercicioLucas
+{
+    class Program
+    {
+        private static int i;
+
+        static void Main(string[] args)
+        {
+            string[] churras = new string[5] {"Lucas", "Tony", "Brandon", "Nicolas","Fuscão" };
+            int[] idade = new int[5] { 26, 30, 28, 22, 25 };
+
+
+            Console.WriteLine("Mulekada");
+            for (i = 0; i < churras.Length; i++)
+            {
+                Console.WriteLine((i + 1) + " . " + churras[i].ToUpper() + " - " + idade[i]);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
